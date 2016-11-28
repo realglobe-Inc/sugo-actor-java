f73ce3f768622449e604ed19df7cd5862eda101f
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-11-28 14:29:16 +0900

    接続先のパス指定が効くようにした

--- src/main/java/jp/realglobe/sugo/actor/Actor.java
+++ src/main/java/jp/realglobe/sugo/actor/Actor.java
@@ -66,7 +66,18 @@ public class Actor {
             LOG.info("Already connected");
             return;
         }
-        this.socket = (new Manager(URI.create(this.hub))).socket(NAMESPACE);
+
+        final URI hubUri = URI.create(this.hub);
+        final Manager.Options options = new Manager.Options();
+        options.hostname = hubUri.getHost();
+        options.port = hubUri.getPort();
+        options.path = hubUri.getPath();
+        if (!options.path.endsWith("/")) {
+            options.path += "/";
+        }
+        options.path += "socket.io";
+        options.secure = hubUri.getScheme().equals("https");
+        this.socket = (new Manager(options)).socket(NAMESPACE);
 
         this.socket.on(Socket.EVENT_CONNECT, args -> {
             LOG.fine("Connected to " + this.hub);
